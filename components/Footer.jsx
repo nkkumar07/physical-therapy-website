aaa01e5b3c8f73cb42477b64adca6b6ad759631a
nkkumar07 <nkkumawat7357@gmail.com>
2025-06-02 09:22:32 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      Footer Start
       <div className="container-fluid footer py-5 wow fadeIn" data-wow-delay="0.2s">
         <div className="container py-5">
           <div className="row g-5">
